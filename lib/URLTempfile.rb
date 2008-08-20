d386d670c01ad19d17813de830fe5c15fff83b7c
Christopher Bailey <chris@cobaltedge.com>
2008-08-20 14:21:53 -0700

    Convert implementation to use open-uri.  This solves missing redirect support.  However, it introduces the need for a hack to OpenSSL so that certificate validation isn't used.  Hack is to temporarily set no certification validation globally for OpenSSL while the file is downloaded.  But BEWARE, this means that if you have other HTTPS requests that need certificate validation occurring at the same time, they will be affected.

--- lib/URLTempfile.rb
+++ lib/URLTempfile.rb
@@ -1,5 +1,7 @@
 require 'mime/types'
-require 'net/https'
+require 'openssl'
+require 'open-uri'
+
 
 # This class provides a Paperclip plugin compliant interface for an "upload" file
 # where that uploaded file is actually coming from a URL.  This class will download
@@ -7,27 +9,31 @@ require 'net/https'
 # as required by Paperclip so that the file can be processed and managed by 
 # Paperclip just as a regular uploaded file would.
 #
-# TODO: support redirects
-# TODO: handle lack of OpenSSL/HTTPS
-# TODO: if identify app not available, use file extension (if avail) or other 
-#       mechanism to get content_type
 class URLTempfile < Tempfile
-    
+  attr :content_type
+  
   def initialize(url)
     @url = URI.parse(url)
     
     # see if we can get a filename
     raise "Unable to determine filename for URL uploaded file." unless original_filename
-    
-    # Create a tempfile and download the image specified in the URL into it
-    super('urlupload')
-    request = Net::HTTP.new(@url.host, @url.port)
-    request.use_ssl = (@url.scheme == 'https')
-    request.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    self.write request.get(@url.request_uri).body
-    self.flush
-    
-    raise "Unable to determine MIME type for URL uploaded file." unless content_type
+
+    begin
+      # HACK to get around inability to set VERIFY_NONE with open-uri
+      old_verify_peer_value = OpenSSL::SSL::VERIFY_PEER
+      OpenSSL::SSL.const_set("VERIFY_PEER", OpenSSL::SSL::VERIFY_NONE)
+      
+      super('urlupload')
+      Kernel.open(url) do |file|
+        @content_type = file.content_type
+        raise "Unable to determine MIME type for URL uploaded file." unless content_type
+      
+        self.write file.read
+        self.flush
+      end
+    ensure
+      OpenSSL::SSL.const_set("VERIFY_PEER", old_verify_peer_value)
+    end
   end
   
   def original_filename
@@ -36,17 +42,4 @@ class URLTempfile < Tempfile
     match = @url.path.match(/^.*\/(.+)$/)
     return (match ? match[1] : nil)
   end
-  
-  def content_type
-    begin
-      info = `identify "#{path}"`
-      Rails.logger.debug "identify results for (#{path}): #{info}"
-      raise "Failed running identify on #{@url}" if info.empty?
-      return MIME::Types.type_for(info.split[1]).first.simplified
-    rescue => e
-      Rails.logger.error "Unable to determine content type for URL uploaded file: #{e.message}"
-      nil
-    end
-  end
-    
 end
